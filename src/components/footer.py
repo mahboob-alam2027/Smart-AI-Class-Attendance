1a4f5df18c10241f622e38cd025f8c9853589008
Mahboob Alam <me.alam2027@gmail.com>
2026-09-18 22:36:02 +0530

    Finishing Touches

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,14 +2,20 @@ import streamlit as st
 
 def footer_home():
 
-    logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
-
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
         <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>
         <span style="font-weight:bold; color:#E0E3FF; font-size:1rem;">Mahboob Alam</span>
         </div>
-
-
-
                 """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    st.markdown(
+        """
+        <div style='text-align: center; padding: 10px; font-weight: bold;'>
+            Created with ❤️ by <span style='color: Black;'>Mahboob Alam</span>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
