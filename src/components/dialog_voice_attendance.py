fa828a0df69eb1ede257110b4df8a7e0ee4fcc21
Mahboob Alam <me.alam2027@gmail.com>
2026-09-20 22:59:49 +0530

    Initial commit with updated AI attendance codebase

--- src/components/dialog_voice_attendance.py
+++ src/components/dialog_voice_attendance.py
@@ -3,7 +3,7 @@ from src.pipelines.voice_pipeline import process_bulk_audio
 from src.database.config import supabase
 from datetime import datetime
 import pandas as pd
-from src.components.dialog_attendance_results import show_attendance_result
+from src.components.dialog_attendance_results import show_attendance_results
 
 
 
